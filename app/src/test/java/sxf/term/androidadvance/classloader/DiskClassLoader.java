3322f176899471176e6be9a7f77dd47fb3c7a308
s5457d <596541901@qq.com>
2019-04-23 10:54:58 +0800

    添加自定义的classloader。 添加main，定义如何调用自定义的classloader。

---
+++ app/src/test/java/sxf/term/androidadvance/classloader/DiskClassLoader.java
@@ -0,0 +1,80 @@
+package sxf.term.androidadvance.classloader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author by sunzhongda
+ * @date 2019/4/23
+ */
+public class DiskClassLoader extends ClassLoader {
+
+    private String path;
+
+    public DiskClassLoader(String path) {
+        this.path = path;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        Class clazz = null;
+        byte[] classData = loadClassData(name);
+        if (classData == null) {
+            throw new ClassNotFoundException();
+        } else {
+            clazz = defineClass(name, classData, 0, classData.length);
+        }
+        return clazz;
+    }
+
+    private byte[] loadClassData(String name) {
+        String fileName = getFileName(name);
+        File file = new File(path, fileName);
+        InputStream in = null;
+        ByteArrayOutputStream out = null;
+
+        try {
+            in = new FileInputStream(file);
+            out = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int length = 0;
+            while ((length = in.read(buffer)) != -1) {
+                out.write(buffer);
+            }
+            return out.toByteArray();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    private String getFileName(String name) {
+        int index = name.lastIndexOf('.');
+        if (index == -1) {
+            return name + ".class";
+        } else {
+            return name.substring(index + 1) + ".class";
+        }
+    }
+}
---
+++ app/src/test/java/sxf/term/androidadvance/classloader/main.java
@@ -0,0 +1,37 @@
+package sxf.term.androidadvance.classloader;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author by sunzhongda
+ * @date 2019/4/23
+ */
+public class main {
+
+    @Test
+    public void main(){
+        DiskClassLoader diskClassLoader = new DiskClassLoader("/Users/sunzhongda/Desktop");
+        try {
+            Class<?> c = diskClassLoader.loadClass("sxf.term.androidadvance.classloader.Jobs");
+            if (c!=null){
+                Object obj = c.newInstance();
+                System.out.println(obj.getClass().getClassLoader());
+                Method method = c.getDeclaredMethod("say", null);
+                method.invoke(obj, null);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
