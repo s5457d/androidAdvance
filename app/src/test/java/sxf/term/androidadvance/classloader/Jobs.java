8a34efebb4d14849fae0a0c8604ab279a43b2baf
s5457d <596541901@qq.com>
2019-04-23 10:58:19 +0800

    注意如果项目里面已经存在jobs.java，默认会调用appclassloader。 如果想调用自定义的classloader，需要项目里面不存在jobs.java。

---
+++ app/src/test/java/sxf/term/androidadvance/classloader/Jobs.java
@@ -0,0 +1,13 @@
+package sxf.term.androidadvance.classloader;
+
+/**
+ * @author by sunzhongda
+ * @date 2019/4/23
+ */
+public class Jobs {
+
+    public void say(){
+        System.out.println("One more thing");
+    }
+
+}
