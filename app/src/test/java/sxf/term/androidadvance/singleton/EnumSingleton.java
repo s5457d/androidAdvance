3f10a83aefb7a4a585a4a14bfab8b7c97e0c5879
s5457d <596541901@qq.com>
2019-06-28 11:28:13 +0800

    可以保证线程安全，写法简单。

---
+++ app/src/test/java/sxf/term/androidadvance/singleton/EnumSingleton.java
@@ -0,0 +1,12 @@
+package sxf.term.androidadvance.singleton;
+
+/**
+ * @author by sunzhongda
+ * @date 2019-06-28
+ */
+public enum  EnumSingleton {
+
+    INSTANCE;
+
+    public void doSomeThing(){}
+}
